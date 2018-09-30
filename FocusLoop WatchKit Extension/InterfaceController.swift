87ecdd2fa4be29b2cbc53c47265bddf5698d02c7
avijeet <a@avijeets.com>
2018-09-29 22:30:46 -0400

    Initial Commit

---
+++ FocusLoop WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FocusLoop WatchKit Extension
+//
+//  Created by Avijeet Sachdev on 9/29/18.
+//  Copyright © 2018 Avijeet Sachdev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
