cc6359d67b105218dd0df65fa587ebd5c59a6d3b
avijeet <a@avijeets.com>
2018-10-07 23:20:27 -0400

    notifications fully functional now, working on proper routing procedure

--- FocusLoop WatchKit Extension/NotificationsInterfaceController.swift
+++ FocusLoop WatchKit Extension/NotificationsInterfaceController.swift
@@ -11,7 +11,7 @@ import Foundation
 import UserNotifications
 
 class NotificationsInterfaceController: WKInterfaceController {
-
+    var hours = 1
     @IBOutlet weak var timePicker: WKInterfacePicker!
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -35,25 +35,36 @@ class NotificationsInterfaceController: WKInterfaceController {
     }
 
     @IBAction func startNotif() {
-        let content = UNMutableNotificationContent()
-        content.body = "Are you still productive?"
-        content.categoryIdentifier = "focusedCategory"
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: NSUUID().uuidString, content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add( request) { (error) in
-            if error != nil {
-                print("Error caught")
-            }
-            else {
-                print("No error found")
+        for hour in 1...hours {
+            let content = UNMutableNotificationContent()
+            content.body = "Are you still productive?"
+            content.categoryIdentifier = "focusedCategory"
+            let secondRound = 60 * 60 * hour
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(secondRound), repeats: false)
+            let request = UNNotificationRequest(identifier: NSUUID().uuidString, content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request) { (error) in
+                if error != nil {
+                    print("Error caught")
+                }
+                else {
+                    print("No error found")
+                }
             }
         }
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (requests) in
+            print(requests.count)
+        }
     }
+    @IBAction func deleteTapped() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
 
     @IBAction func pickerChanged(_ value: Int) {
+        hours = value + 1
     }
 }
